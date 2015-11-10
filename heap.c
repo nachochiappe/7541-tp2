2e913388afc91b5478668f5e896f0ca85ef53f72
Ignacio Chiappe <nacho.chiappe@gmail.com>
2015-11-10 09:46:04 -0300

    Nueva versión del heap.

--- heap.c
+++ heap.c
@@ -3,188 +3,144 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-        // Definicion estructuras //
+#define TAM_INICIAL 5
+#define FACTOR_AUMENTAR 0.7
+#define AUMENTAR_TAM 3
 
-typedef struct nodo_heap nodo_heap_t;
-
-struct nodo_heap{
-	void* dato;
-	nodo_heap_t* izq;
-	nodo_heap_t* der;
-    nodo_heap_t* padre;
-};
+		// Definicion estructuras //
 
 struct heap{
-    size_t cantidad;
-    nodo_heap_t* raiz;
-    cmp_func_t cmp;
+	size_t cantidad;
+	size_t tamanio;
+	void** tabla_heap;
+	cmp_func_t cmp;
 };
 
+		// Funciones Auxiliares //
 
-        // Funciones Auxiliares //
-
-/* Crea un nuevo nodo para el heap, segun el elemento
- * pasado por parametro. En caso de fallar, devuelve
- * NULL, si no, un puntero al nuevo nodo.
- */
-nodo_heap_t* crear_nodo_heap(void* elemento){
-    nodo_heap_t* nuevo_nodo = malloc(sizeof(nodo_heap_t));
-    if (!nuevo_nodo) return NULL;
-    nuevo_nodo->dato = elemento;
-    nuevo_nodo->izq = NULL;
-    nuevo_nodo->der = NULL;
-    nuevo_nodo->padre = NULL;
-    return nuevo_nodo;
-}
 
 /* Ordena el heap de forma recursiva hacia arriba, segun
  * el orden de prioridad establecido en la funcion 'heap->cmp'.
  */
-void up_heap(heap_t* heap, nodo_heap_t* actual){
-    nodo_heap_t* padre = actual->padre;
-    if (!padre) return; // El actual es la raiz del heap (es maximo).
-    int comparar_claves = heap->cmp(actual->dato, padre->dato);
-    if (comparar_claves > 0){ // La clave del actual es mayor a la de su padre.
-        void* aux_dato = actual->dato;
-        actual->dato = padre->dato;
-        padre->dato = aux_dato;
-        actual = padre;
-        up_heap(heap, actual);
-    }
-    // En otro caso (menor o igual), dejamos el heap como esta.
+void up_heap(heap_t* heap, size_t pos_actual){
+	if (pos_actual == 0) return; // El actual es la raiz del heap (es maximo).
+	void* actual = heap->tabla_heap[pos_actual];
+	void* padre = heap->tabla_heap[(pos_actual - 1)/ 2];
+	int comparar_claves = heap->cmp(actual, padre);
+	if (comparar_claves > 0){ // La clave del actual es mayor a la de su padre.
+		heap->tabla_heap[pos_actual] = padre;
+		heap->tabla_heap[(pos_actual - 1)/ 2] = actual;
+		pos_actual = (pos_actual - 1)/ 2;
+		up_heap(heap, pos_actual);
+	}
+	// En otro caso (menor o igual), dejamos el heap como esta.
 }
 
-/* Busca y devuelve un padre sin alguno de los dos hijos.
- * Al encontrarlo, lo devuelve. En caso de tener un solo
- * nodo (un heap solo con la raiz), devuelve NULL.
+/* Aumenta el tamanio del heap segun AUMENTAR_TAM. Devuelve
+ * true si pudo copiar los datos en la tabla nueva, o false si no.
  */
-nodo_heap_t* buscar_padre(heap_t* heap){
-    cola_t* cola_en_nivel = cola_crear();
-    if (!cola_en_nivel) return NULL;
-    nodo_heap_t* padre = heap->raiz; // Se controla en 'heap_encolar' que exista (el heap no esta vacio).
-    while (padre){
-        if (!padre->izq || !padre->der) break;
-        cola_encolar(cola_en_nivel, padre->izq);
-        cola_encolar(cola_en_nivel, padre->der);
-        padre = cola_desencolar(cola_en_nivel);
-    }
-    cola_destruir(cola_en_nivel, NULL);
-    return padre;
+bool redimensionar_heap(heap_t* heap){
+	size_t nuevo_tam = heap->tamanio * AUMENTAR_TAM;
+	void** tabla_nueva = realloc(heap->tabla_heap, nuevo_tam * sizeof(void*));
+	if (!tabla_nueva) return false;
+	heap->tabla_heap = tabla_nueva;
+	heap->tamanio = nuevo_tam;
+	return true;
 }
 
-/* Inserta, en el lugar correspondiente, el nodo pasado por
+
+/* Inserta en la ultima posicion el elemento pasado por
  * parametro. En caso de fallar, devuelve false. Si no, devuelve
  * true, habiendo ordenado el heap de forma tal que se mantenga
- * la prioridad establecida luego de insertar el nodo.
+ * la prioridad establecida luego de insertar el valor.
  */
-bool insertar_con_prioridad(heap_t* heap, nodo_heap_t* nodo){
-    // Primero insertamos al final del heap (cuando se encuentra la hoja vacia correspondiente).
-    nodo_heap_t* auxiliar = buscar_padre(heap);
-    if (!auxiliar) return false;
-    if (!auxiliar->izq)
-        auxiliar->izq = nodo;
-    else
-        auxiliar->der = nodo;
-    heap->cantidad++;
-    nodo->padre = auxiliar;
-    // Ordenamos el heap segun la prioridad dada por la funcion cmp.
-    up_heap(heap, nodo);
-    return true;
+bool insertar_con_prioridad(heap_t* heap, void* elemento){
+	if (heap->cantidad / heap->tamanio >= FACTOR_AUMENTAR){
+		bool redimension = redimensionar_heap(heap);
+		if (heap->cantidad / heap->tamanio == 1 && !redimension) return false;
+	}
+	heap->tabla_heap[heap->cantidad] = elemento;
+	up_heap(heap, heap->cantidad);
+	heap->cantidad++;
+	return true;
 }
 
-/* Busca y devuelve el ultimo nodo del heap, siendo este en el
- * nivel mas inferior y a la derecha del mismo. En caso de fallar,
- * devuelve NULL.
+/* Devuelve el mayor de los hijos del valor en la posicion pasada.
+ * En caso de no tener hijos izquierdos, devuelve la cantidad del heap
+ * (es un nodo hoja). Caso contrario, devuelve la posicion correspondiente.
  */
-nodo_heap_t* buscar_ultima_hoja(heap_t* heap){
-    cola_t* cola_en_nivel = cola_crear();
-    if (!cola_en_nivel) return NULL;
-    nodo_heap_t* ultima_hoja = heap->raiz; // Se controla en 'heap_desencolar' que exista (el heap no esta vacio).
-    do{
-        if (ultima_hoja->izq)
-            cola_encolar(cola_en_nivel, ultima_hoja->izq);
-        if (ultima_hoja->der)
-            cola_encolar(cola_en_nivel, ultima_hoja->der);
-        ultima_hoja = cola_desencolar(cola_en_nivel);
-    }while (!cola_esta_vacia(cola_en_nivel));
-    cola_destruir(cola_en_nivel, NULL);
-    return ultima_hoja;
+size_t buscar_max_hijos(heap_t* heap, size_t pos_actual){
+	size_t pos_hijo_izq = 2 * pos_actual + 1;
+	size_t pos_hijo_der = pos_hijo_izq + 1;
+	if (pos_hijo_izq >= heap->cantidad) return heap->cantidad; // Final del arbol
+	if (pos_hijo_der >= heap->cantidad) return pos_hijo_izq;
+	int comparacion = heap->cmp(heap->tabla_heap[pos_hijo_izq], heap->tabla_heap[pos_hijo_der]);
+	if (comparacion > 0) return pos_hijo_izq;
+	return pos_hijo_der;
 }
 
-/* Devuelve el mayor de los hijos de un nodo dado por parametro.
- * En caso de no tener hijos izquierdos, devuelve NULL (es un nodo
- * hoja). Caso contrario, devuelve el nodo correspondiente.
- */
-nodo_heap_t* buscar_max_hijos(heap_t* heap, nodo_heap_t* padre){
-    if (!padre->izq) return NULL; // El nodo padre pasado no tiene hijos.
-    if (!padre->der) return padre->izq;
-    int comparacion = heap->cmp(padre->izq->dato, padre->der->dato);
-    if (comparacion >= 0) return padre->izq;
-    return padre->der;
-}
-
-/* Ordena el heap desde el nodo pasado por parametro hacia abajo
+/* Ordena el heap desde la posicion pasada por parametro hacia abajo
  * (en caso de ser necesario) segun la funcion de comparacion dada.
  */
-void down_heap(heap_t* heap, nodo_heap_t* actual){
-    nodo_heap_t* hijo_mayor = buscar_max_hijos(heap, actual);
-    if (!hijo_mayor) return; // Sucede al llegar al final del arbol.
-    if (heap->cmp(actual->dato, hijo_mayor->dato) < 0){
-        void* dato = actual->dato;
-        actual->dato = hijo_mayor->dato;
-        hijo_mayor->dato = dato;
-        down_heap(heap, hijo_mayor);
-    }
+void down_heap(heap_t* heap, size_t pos_actual){
+	size_t pos_hijo_mayor = buscar_max_hijos(heap, pos_actual);
+	if (pos_hijo_mayor == heap->cantidad) return; // Sucede al llegar al final del arbol.
+	void* actual = heap->tabla_heap[pos_actual];
+	void* hijo_mayor = heap->tabla_heap[pos_hijo_mayor];
+	if (heap->cmp(actual, hijo_mayor) < 0){
+		heap->tabla_heap[pos_actual] = hijo_mayor;
+		heap->tabla_heap[pos_hijo_mayor] = actual;
+		down_heap(heap, pos_hijo_mayor);
+	}
 }
 
-/* Elimina el nodo de mayor prioridad en el heap. Se verifica
+/* Elimina el elemento de mayor prioridad en el heap. Se verifica
  * que el heap quede ordenado por prioridad luego del borrado.
- * Finalmente, devuelve el elemento asociado al nodo.
+ * Finalmente, devuelve el elemento.
  */
 void* eliminar_primero(heap_t* heap){
-    void* elemento = heap->raiz->dato;
-    nodo_heap_t* ultima_hoja = buscar_ultima_hoja(heap);
-    if (!ultima_hoja) return NULL;
-    heap->raiz->dato = ultima_hoja->dato;
-    nodo_heap_t* padre_ultima_hoja = ultima_hoja->padre;
-    if (padre_ultima_hoja->izq == ultima_hoja)
-        padre_ultima_hoja->izq = NULL;
-    else
-        padre_ultima_hoja->der = NULL;
-    free(ultima_hoja);
-    heap->cantidad--;
-    down_heap(heap, heap->raiz);
-    return elemento;
+	void* elemento = heap->tabla_heap[0];
+	heap->tabla_heap[0] = heap->tabla_heap[heap->cantidad - 1];
+	heap->tabla_heap[heap->cantidad - 1] = NULL;
+	heap->cantidad--;
+	down_heap(heap, 0);
+	return elemento;
 }
 
-        // Primitivas Heap //
+		// Primitivas Heap //
 
 /* Crea un heap. Recibe como único parámetro la función de comparación a
  * utilizar. Devuelve un puntero al heap, el cual debe ser destruido con
  * heap_destruir(), o NULL en caso de fallar.
  */
 heap_t* heap_crear(cmp_func_t cmp){
-    heap_t* heap = malloc(sizeof(heap_t));
-    if (!heap) return NULL;
-    heap->cantidad = 0;
-    heap->raiz = NULL;
-    heap->cmp = cmp;
-    return heap;
+	heap_t* heap = malloc(sizeof(heap_t));
+	if (!heap) return NULL;
+	void** tabla_heap = malloc(sizeof(void*) * TAM_INICIAL);
+	if (!tabla_heap){
+		free(heap);
+		return NULL;
+	}
+	heap->cantidad = 0;
+	heap->tamanio = TAM_INICIAL;
+	heap->tabla_heap = tabla_heap;
+	heap->cmp = cmp;
+	return heap;
 }
 
 /* Devuelve true si la cantidad de elementos que hay en el heap es 0,
  * false en caso contrario.
  */
 bool heap_esta_vacio(const heap_t* heap){
-    if (!heap) return true;
-    return heap->cantidad == 0;
+	if (!heap) return true;
+	return heap->cantidad == 0;
 }
 
 /* Devuelve la cantidad de elementos que hay en el heap.
  */
 size_t heap_cantidad(const heap_t* heap){
-    if (heap_esta_vacio(heap)) return 0; // En caso de pasar un heap invalido, la funcion 'heap_esta_vacio' devuelve true.
-    return heap->cantidad;
+	if (heap_esta_vacio(heap)) return 0; // En caso de pasar un heap invalido, la funcion 'heap_esta_vacio' devuelve true.
+	return heap->cantidad;
 }
 
 /* Agrega un elemento al heap. El elemento no puede ser NULL.
@@ -193,15 +149,13 @@ size_t heap_cantidad(const heap_t* heap){
  * Post: se agregó un nuevo elemento al heap.
  */
 bool heap_encolar(heap_t* heap, void* elem){
-    if (!elem || !heap) return false;
-    nodo_heap_t* nuevo_nodo = crear_nodo_heap(elem);
-    if (!nuevo_nodo) return false;
-    if (heap_esta_vacio(heap)){
-        heap->raiz = nuevo_nodo;
-        heap->cantidad++;
-        return true;
-    }
-    return insertar_con_prioridad(heap, nuevo_nodo);
+	if (!elem || !heap) return false;
+	if (heap_esta_vacio(heap)){
+		heap->tabla_heap[0] = elem;
+		heap->cantidad++;
+		return true;
+	}
+	return insertar_con_prioridad(heap, elem);
 }
 
 /* Devuelve el elemento con máxima prioridad. Si el heap esta vacío, devuelve
@@ -209,8 +163,8 @@ bool heap_encolar(heap_t* heap, void* elem){
  * Pre: el heap fue creado.
  */
 void* heap_ver_max(const heap_t* heap){
-    if (heap_esta_vacio(heap)) return NULL;
-    return heap->raiz->dato;
+	if (heap_esta_vacio(heap)) return NULL;
+	return heap->tabla_heap[0];
 }
 
 /* Elimina el elemento con máxima prioridad, y lo devuelve.
@@ -219,14 +173,14 @@ void* heap_ver_max(const heap_t* heap){
  * Post: el elemento desencolado ya no se encuentra en el heap. 
  */
 void* heap_desencolar(heap_t* heap){
-    if (heap_esta_vacio(heap)) return NULL; // El heap esta vacio o es invalido.
-    if (heap->cantidad == 1){
-        void* elemento = heap->raiz->dato;
-        free(heap->raiz);
-        heap->cantidad--;
-        return elemento;
-    }
-    return eliminar_primero(heap);
+	if (heap_esta_vacio(heap)) return NULL; // El heap esta vacio o es invalido.
+	if (heap->cantidad == 1){
+		void* elemento = heap->tabla_heap[0];
+		heap->tabla_heap[0] = NULL;
+		heap->cantidad--;
+		return elemento;
+	}
+	return eliminar_primero(heap);
 }
 
 /* Elimina el heap, llamando a la función dada para cada elemento del mismo.
@@ -235,29 +189,29 @@ void* heap_desencolar(heap_t* heap){
  * dejó de ser válido.
  */
 void heap_destruir(heap_t* heap, void destruir_elemento(void* e)){
-    while (!heap_esta_vacio(heap)){
-        void* valor_a_borrar = heap_desencolar(heap);
-        if (destruir_elemento) destruir_elemento(valor_a_borrar);
-    }
-    free(heap);
+	while (!heap_esta_vacio(heap)){
+		void* valor_a_borrar = heap_desencolar(heap);
+		if (destruir_elemento) destruir_elemento(valor_a_borrar);
+	}
+	free(heap->tabla_heap);
+	free(heap);
 }
 
-        // Primitiva Heap Sort //
+		// Primitiva Heap Sort //
 
 /* Función de heapsort genérica. Esta función ordena mediante heap_sort
  * un arreglo de punteros opacos, para lo cual requiere que se
  * le pase una función de comparación. Modifica el arreglo "in-place".
- * Notar que esta función NO es formalmente parte del TAD Heap.
  */
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
-    heap_t* arreglo_heap = heap_crear(cmp);
-    if (!arreglo_heap) return;
-    int i;
-    for (i = 0; i < cant; i++){
-        heap_encolar(arreglo_heap, elementos[i]);
-    } // Tenemos el heap del arreglo ordenado segun la prioridad dada.
-    for (i = 0; i < cant; i++){
-        elementos[i] = heap_desencolar(arreglo_heap);
-    }
-    heap_destruir(arreglo_heap, NULL);
+	heap_t* arreglo_heap = heap_crear(cmp);
+	if (!arreglo_heap) return;
+	size_t i;
+	for (i = 0; i < cant; i++){
+		heap_encolar(arreglo_heap, elementos[i]);
+	} // Tenemos el heap del arreglo ordenado segun la prioridad dada.
+	for (i = 0; i < cant; i++){
+		elementos[cant - 1 - i] = heap_desencolar(arreglo_heap);
+	}
+	heap_destruir(arreglo_heap, NULL);
 }
